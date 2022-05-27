728e35a5fedf60dafd55ee60f7192419f7c89fbd
GRangel-Dev <rantro.dev@gmail.com>
2022-05-27 19:41:53 -0300

    exercício para verificar se as palavras ao contrário tem o mesmo significado

---
+++ script.js
@@ -0,0 +1,26 @@
+// SOLUÇÃO 1:
+function verificaPalindrono(string){
+     if(!string) return "string inexistente";
+
+     return string.split("").reverse().join("") === string;
+}
+
+console.log(verificaPalindrono("ama"));
+
+// SOLUÇÃO 2
+function verificaPalindrono2(string){
+     if(!string) return "string inexistente";
+     
+     for(let i = 0; i < string.length; i ++){
+        if(string[i] !== string[string.length -1 -i]){
+             return false;
+
+        } 
+
+     }
+
+     return true;
+}
+
+console.log(verificaPalindrono2("abba"));
+
